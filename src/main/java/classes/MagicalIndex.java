df119db0e673e13d65ea9b6ed5e0cc2d8d391a5d
BrettREvans <Brett704@revature.net>
2022-05-11 11:46:03 -0700

    comments describing the process written within the methods of MagicalIndex

--- src/main/java/classes/MagicalIndex.java
+++ src/main/java/classes/MagicalIndex.java
@@ -2,8 +2,9 @@ package classes;
 
 public class MagicalIndex {
 
-
+    //brute force method to find magic index within a sorted int array
     public int magicIndexBruteTechnique(int[] array) {
+        //compare every value in the array against its own position only stoping when a magic index is found
         for (int i = 0; i < array.length; i++) {
             if (array[i] == i) {
                 return i;
@@ -14,8 +15,20 @@ public class MagicalIndex {
 
     //recursive method using binary search pattern to find magic index within a sorted int array
     public int magicIndexBinarySearch(int[] arr, int start, int end) {
+        //array need to be sorted to work
+        //  if start is less or equal to end we can search for a magic index still
+        //  otherwise the array has no magic index and returns -1
         if (start <= end) {
+            //save the mid point of the array
+            // if that midpoint is equal to the value given
+            //                                             we found or magic index
+            // else if mid is greater than the value given
+            //                                             we should check the right side
+            // else the mid is less than the value given
+            //                                             we should check the left side
+
             int mid = (start + end) /2;
+
             if (mid == arr[mid]){
                 return mid;
             }else if (mid > arr[mid]){
@@ -26,8 +39,6 @@ public class MagicalIndex {
                 return magicIndexBinarySearch(arr, start, mid - 1);
             }
         }
-
         return -1;
-
     }
 }
